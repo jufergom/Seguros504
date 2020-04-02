1d7b4b3c24863694e41950fd8e0bce2f0e255148
francomidence <midencefranco@gmail.com>
2020-04-01 19:11:28 -0600

    Added RUD operations on insurance

--- backend/router/insurance.js
+++ backend/router/insurance.js
@@ -30,8 +30,38 @@ router.route('/add').post((req, res) => {
 
   newInsurance
     .save()
-    .then(() => res.json('Exercise added'))
+    .then(() => res.json('Insurance added'))
     .catch(err => res.status(400).json('Error' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Insurance.findById(req.param.id)
+    .then(insurance => res.json(insurance))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Insurance.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Insurance deleted.'))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Insurance.findById(req.param.id).then(insurance => {
+    insurance.name = req.body.name;
+    insurance.description = req.body.description;
+    insurance.points = req.body.points;
+    insurance.primaryMed = req.body.primaryMed;
+    insurance.refund = req.body.refund;
+    insurance.urgency = req.body.urgency;
+    insurance.specialty = req.body.specialty;
+    insurance.diagnostics = req.body.diagnostics;
+
+    insurance
+      .save()
+      .then(() => res.json('Insurance updated'))
+      .catch(err => res.status(400).json('Error ' + err));
+  });
 });
 
 module.exports = router;
