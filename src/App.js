8e4ac7b6bbb64c18c67bd53bd24facb63bf782a8
francomidence <midencefranco@gmail.com>
2020-04-02 00:09:11 -0600

    Created Landing Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import LandingPage from './views/LandingPage/LandingPage';
 
 const routing = (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route path="/" exact component={Home}></Route>
+        <Route path="/" exact component={LandingPage}></Route>
       </Switch>
     </div>
   </BrowserRouter>
 );
 
 function App() {
-  return (
-    <div className="App">
-      {routing}
-    </div>
-  );
+  return <div className="App">{routing}</div>;
 }
 
 export default App;
---
+++ src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.js
@@ -0,0 +1,19 @@
+import { conatinerFluid } from '../../../material-kit-react';
+
+import imagesStyle from '../../../material-kit-react/imagesStyles';
+
+const exampleStyle = {
+  section: {
+    padding: '70px 0'
+  },
+  container: {
+    ...conatinerFluid,
+    textAlign: 'center !important'
+  },
+  ...imagesStyle,
+  link: {
+    textDecoration: 'none'
+  }
+};
+
+export default exampleStyle;
